418c501e23f678aaff16bee20ceb054c87f0420b
Kanggwanju <xprtktm1@gmail.com>
2025-08-25 10:41:47 +0900

    1-3-4. 컴포넌트 기초

---
+++ src/components/Fruit.jsx
@@ -0,0 +1,16 @@
+// rsc
+import React from 'react';
+
+const Fruit = () => {
+  return (
+    <div>
+      <ul>
+        <li>사과</li>
+        <li>딸기</li>
+        <li>오렌지</li>
+      </ul>  
+    </div>
+  );
+};
+
+export default Fruit;
