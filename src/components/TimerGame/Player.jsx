c7553acc0658cbca36fc21983bed8e111b8ba664
Kanggwanju <xprtktm1@gmail.com>
2025-08-28 14:06:14 +0900

    6장 복습예제. 2-사용자 이름 세팅

--- src/components/TimerGame/Player.jsx
+++ src/components/TimerGame/Player.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 
 const Player = () => {
+
+  // useRef로 태그 기억시키기
+  const nameRef = useRef(null);
+
+  const [enteredName, setEnteredName] = useState('anonymous');
+
+  const nameChangeHandler = () => {
+    setEnteredName(nameRef.current.value);
+    nameRef.current.value = '';
+  };
+
   return (
     <section id='player'>
-      <h2>Welcome anonymous!</h2>
+      <h2>Welcome {enteredName}!</h2>
       <p>
-        <input type='text' />
-        <button>Set Name</button>
+        <input
+          type='text'
+          ref={nameRef}
+        />
+        <button onClick={nameChangeHandler}>Set Name</button>
       </p>
     </section>
   );
